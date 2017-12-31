67bdaad17272f984477780040fadf9ae69284de1
Jessica <email@email.com>
2017-12-30 19:38:18 -0500

    Creating a red left autonomous program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAutonomous.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
+
+@Autonomous(name = "Left Red Autonomous", group = "Red")
+public class RedLeftAutonomous extends LinearOpMode {
+
+    //
+    // Creating the robot class from the Plan B Hardware class and passing in telemetry
+    // object as a parameter so that the hardware class can spit out telemetry data
+    //
+    RobotHardware         robot   = new RobotHardware(telemetry);
+    Jewel                 jewel   = new Jewel(robot, telemetry);
+    Forward forward = new Forward(robot,telemetry);
+    Gyro gyro = new Gyro(robot, telemetry);
+    ArmHandler armHandler = new ArmHandler(robot, telemetry);
+    ColorSensorSlide colorSensorSlide = new ColorSensorSlide(robot, telemetry);
+    Glyph glyph = new Glyph(robot, telemetry);
+    GyroForward gyroForward = new GyroForward(robot, telemetry);
+    Slide slide = new Slide(robot, telemetry);
+    ConceptVuMarkId conceptVuMarkId = null;
+
+    public void runOpMode() throws InterruptedException {
+        double distanceForward;
+        robot.init(hardwareMap);
+        conceptVuMarkId = new ConceptVuMarkId(hardwareMap, telemetry);
+        RelicRecoveryVuMark column = null;
+
+        waitForStart();
+        robot.gyroSensor.resetZAxisIntegrator();
+        jewel.JewelSwatter(robot.RED);
+        column = conceptVuMarkId.findColumn(5000);
+        glyph.grabber(robot.CLOSE);
+        armHandler.armToPosition(400);
+
+        gyroForward.distance(-30);
+        slide.run(0.25, 3, robot.RIGHT);
+        gyro.turn(90);
+        gyroForward.sonic(14);
+        colorSensorSlide.findColumn(robot.RED, column, robot.LEFT);
+        gyro.turn(90);
+        forward.run(0.25, 8);
+        glyph.grabber(robot.SOFT);
+        forward.run(0.25, -8);
+
+        armHandler.armToPosition(0);
+        glyph.grabber(robot.OPEN);
+
+    }
+
+
+}
