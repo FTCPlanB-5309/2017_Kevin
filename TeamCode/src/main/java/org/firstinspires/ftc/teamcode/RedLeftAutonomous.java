eaa6660e56ae9e3d7e3000cb2f64a7ffbf4d0fc5
Millard <mgold@triad.rr.com>
2018-02-04 09:51:44 -0500

    Reverting back to Jessica Left Red Auto from Trinity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedLeftAutonomous.java
@@ -25,62 +25,39 @@ public class RedLeftAutonomous extends LinearOpMode {
     GyroForward gyroForward = new GyroForward(robot, telemetry);
     Slide slide = new Slide(robot, telemetry);
     ConceptVuMarkId conceptVuMarkId = null;
-    ColorSensorBackward colorSensorBackward = new ColorSensorBackward(robot, telemetry);
 
 
     public void runOpMode() throws InterruptedException {
         double distanceForward;
         robot.init(hardwareMap);
         conceptVuMarkId = new ConceptVuMarkId(hardwareMap, telemetry);
-
         RelicRecoveryVuMark column = null;
         waitForStart();
 
         CameraDevice.getInstance().setFlashTorchMode(true);
 
         robot.gyroSensor.resetZAxisIntegrator();
-//        jewel.JewelSwatter(robot.RED);
+        jewel.JewelSwatter(robot.RED);
         column = conceptVuMarkId.findColumn(4000);
-        CameraDevice.getInstance().setFlashTorchMode(false);
         glyph.close();
-        sleep(300);
-        armHandler.armToPosition(600);
-        backward.run(0.35, 26);
-        telemetry.update();
-        gyro.turn(0);
-        robot.armMotor.setPower(-1);
-        sleep(1000);
-        robot.armMotor.setPower(0);
-        colorSensorBackward.findColumn((int)(-7 * robot.COUNTS_PER_INCH), (int)(7 * robot.COUNTS_PER_INCH),
-                (int)(3* robot.COUNTS_PER_INCH), column);
-        if (column == RelicRecoveryVuMark.LEFT)
-            gyro.turn(50);
-        else if (column == RelicRecoveryVuMark.CENTER)
+        armHandler.armToPosition(500);
+
+        if (column == RelicRecoveryVuMark.RIGHT) {
+            backward.run(0.25, 40);
             gyro.turn(45);
-        else
+        }
+        else if (column == RelicRecoveryVuMark.CENTER) {
+            backward.run(0.25, 49);
             gyro.turn(50);
-        forward.run(0.25, 6);
+        }
+        else if (column == RelicRecoveryVuMark.LEFT) {
+            backward.run(0.25, 29);
+            gyro.turn(210);
+            gyro.turn(125);
+        }
+        forward.run(0.25, 11);
         glyph.soft();
         backward.run(0.25, 6);
         armHandler.armToPosition(0);
-        glyph.open();
-
-//        if (column == RelicRecoveryVuMark.RIGHT) {
-//            backward.run(0.25, 40);
-//            gyro.turn(45);
-//        }
-//        else if (column == RelicRecoveryVuMark.CENTER) {
-//            backward.run(0.25, 49);
-//            gyro.turn(50);
-//        }
-//        else if (column == RelicRecoveryVuMark.LEFT) {
-//            backward.run(0.25, 29);
-//            gyro.turn(210);
-//            gyro.turn(125);
-//        }
-//        forward.run(0.25, 11);
-//        glyph.soft();
-//        backward.run(0.25, 6);
-//        armHandler.armToPosition(0);
     }
 }
